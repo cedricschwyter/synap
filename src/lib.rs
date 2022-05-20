995376ada3d998d1e4e9a05883594a9f3753086e
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-05-20 14:27:28 +0200

    Add matrix-matrix multiplication, matrix-vector multiplication

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
+use num::Num;
+use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign};
 
+pub trait MatrixElement<T>: PartialEq + Debug + Copy {}
+
+impl<T: Num + Debug + Copy> MatrixElement<T> for T {}
+
 #[derive(Debug, PartialEq, Eq)]
-pub struct Matrix<T> {
+pub struct Matrix<T: MatrixElement<T>> {
     elements: Vec<Vec<T>>,
     width: usize,
     height: usize,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Vector<T> {
-    elements: Vec<T>,
-    dimension: usize,
-}
-
-impl<T: Clone + Copy> Matrix<T> {
-    pub fn new(elements: &Vec<Vec<T>>) -> Matrix<T> {
+impl<T: MatrixElement<T>> Matrix<T> {
+    pub fn new(elements: Vec<Vec<T>>) -> Matrix<T> {
         if elements.is_empty() {
             panic!("attempting to create matrix with no elements");
         }
         let height = elements.len();
         let width = elements[0].len();
-        for row in elements {
+        for row in &elements {
             if row.len() != width {
                 panic!(
                     "matrix rows do not contain equal number of elements, expected {}, got {}",
@@ -30,10 +30,35 @@ impl<T: Clone + Copy> Matrix<T> {
             }
         }
         Matrix {
-            elements: elements.to_vec(),
+            elements,
             width,
             height,
         }
+    }
+
+    pub fn vector(elements: Vec<T>) -> Matrix<T> {
+        Matrix::<T>::new(vec![elements]).transpose()
+    }
+
+    pub fn to_scalar(&self) -> T {
+        if self.width != 1 || self.height != 1 {
+            panic!(
+                "cannot convert matrix of size {} x {} to scalar, has to be 1 x 1",
+                self.height, self.width
+            );
+        }
+        self[0][0]
+    }
+
+    pub fn transpose(&self) -> Matrix<T> {
+        let mut elements = Vec::<Vec<T>>::new();
+        for col in 0..self.width {
+            elements.push(Vec::<T>::new());
+            for row in 0..self.height {
+                elements[col].push(self[row][col]);
+            }
+        }
+        Matrix::<T>::new(elements)
     }
 
     fn assert_same_size(&self, rhs: &Self) {
@@ -55,55 +80,7 @@ impl<T: Clone + Copy> Matrix<T> {
     }
 }
 
-impl<T: Add<T, Output = T> + AddAssign<T> + Mul<T, Output = T> + Clone + Copy> Vector<T> {
-    fn new(elements: &Vec<T>) -> Vector<T> {
-        if elements.is_empty() {
-            panic!("vector has dimension 0");
-        }
-        Vector {
-            elements: elements.to_vec(),
-            dimension: elements.len(),
-        }
-    }
-
-    fn assert_same_dimension(&self, rhs: &Vector<T>) {
-        if self.dimension != rhs.dimension {
-            panic!(
-                "vector must be of same dimension to compute scalar product, got {} and {}",
-                self.dimension, rhs.dimension
-            );
-        }
-    }
-}
-
-impl<T: Add<T, Output = T> + AddAssign<T> + Mul<T, Output = T> + Clone + Copy> Mul for Vector<T> {
-    type Output = T;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        self.assert_same_dimension(&rhs);
-        let mut value: T = self[0] * rhs[0];
-        for i in 1..self.dimension {
-            value += self[i] * rhs[i];
-        }
-        value
-    }
-}
-
-impl<T> Index<usize> for Vector<T> {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.elements[index]
-    }
-}
-
-impl<T> IndexMut<usize> for Vector<T> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.elements[index]
-    }
-}
-
-impl<T> Index<usize> for Matrix<T> {
+impl<T: MatrixElement<T>> Index<usize> for Matrix<T> {
     type Output = Vec<T>;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -111,49 +88,56 @@ impl<T> Index<usize> for Matrix<T> {
     }
 }
 
-impl<T> IndexMut<usize> for Matrix<T> {
+impl<T: MatrixElement<T>> IndexMut<usize> for Matrix<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.elements[index]
     }
 }
 
-impl<T: Add<T, Output = E> + Copy, E: Clone + Copy> Add for Matrix<T> {
-    type Output = Matrix<E>;
+impl<T: MatrixElement<T> + Add<T, Output = T>> Add for Matrix<T> {
+    type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
         self.assert_same_size(&rhs);
-        let mut elements = Vec::<Vec<E>>::new();
+        let mut elements = Vec::<Vec<T>>::new();
         for row in 0..self.height {
-            elements.push(Vec::<E>::new());
+            elements.push(Vec::<T>::new());
             for col in 0..self.width {
                 elements[row].push(self[row][col] + rhs[row][col]);
             }
         }
-        Matrix::<E>::new(&elements)
+        Matrix::<T>::new(elements)
     }
 }
 
-impl<T: Neg<Output = E> + Copy, E: Clone + Copy> Neg for Matrix<T> {
-    type Output = Matrix<E>;
+impl<T: MatrixElement<T> + Neg<Output = T>> Neg for Matrix<T> {
+    type Output = Self;
 
     fn neg(self) -> Self::Output {
-        let mut elements = Vec::<Vec<E>>::new();
+        let mut elements = Vec::<Vec<T>>::new();
         for row in 0..self.height {
-            elements.push(Vec::<E>::new());
+            elements.push(Vec::<T>::new());
             for col in 0..self.width {
                 elements[row].push(-self[row][col]);
             }
         }
-        Matrix::<E>::new(&elements)
+        Matrix::<T>::new(elements)
     }
 }
 
-impl<T: Add<T, Output = T> + AddAssign<T> + Mul<T, Output = T> + Clone + Copy> Mul for Matrix<T> {
+impl<T: MatrixElement<T> + Mul<T, Output = T> + AddAssign<T>> Mul for Matrix<T> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
         self.assert_can_multiply(&rhs);
         let mut elements = Vec::<Vec<T>>::new();
+        if self.height == 1 && rhs.width == 1 {
+            let mut value: T = self[0][0] * rhs[0][0];
+            for i in 1..self.width {
+                value += self[0][i] * rhs[i][0];
+            }
+            return Matrix::<T>::vector(vec![value]);
+        }
         for row in 0..self.height {
             elements.push(Vec::<T>::new());
             for col in 0..rhs.width {
@@ -161,10 +145,13 @@ impl<T: Add<T, Output = T> + AddAssign<T> + Mul<T, Output = T> + Clone + Copy> M
                 for i in 0..rhs.height {
                     r.push(rhs[i][col]);
                 }
-                elements[row].push(Vector::<T>::new(&self[row]) * Vector::<T>::new(&r));
+                elements[row].push(
+                    (Matrix::<T>::vector(self[row].to_vec()).transpose() * Matrix::<T>::vector(r))
+                        .to_scalar(),
+                );
             }
         }
-        Matrix::<T>::new(&elements)
+        Matrix::<T>::new(elements)
     }
 }
 
@@ -173,31 +160,54 @@ mod tests {
     use crate::*;
 
     #[test]
+    fn matrix_vector_multiplication() {
+        let left = Matrix::<i32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let right = Matrix::<i32>::vector(vec![9, 10, 11]);
+        assert_eq!(left * right, Matrix::<i32>::vector(vec![32, 122, 212]));
+    }
+
+    #[test]
+    #[should_panic]
+    fn matrix_vector_multiplication_incompatible_size() {
+        let left = Matrix::<i32>::new(vec![vec![0, 1], vec![2, 3], vec![4, 5]]);
+        let right = Matrix::<i32>::vector(vec![6, 7, 8]);
+        let _ = left * right;
+    }
+
+    #[test]
     fn matrix_multiplication() {
-        let left = Matrix::<u32>::new(&vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
-        let right = Matrix::<u32>::new(&vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
+        let left = Matrix::<i32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let right = Matrix::<i32>::new(vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
         assert_eq!(
             left * right,
-            Matrix::<u32>::new(&vec![vec![9, 6, 3], vec![54, 42, 30], vec![99, 78, 57]])
+            Matrix::<i32>::new(vec![vec![9, 6, 3], vec![54, 42, 30], vec![99, 78, 57]])
         );
     }
 
     #[test]
+    #[should_panic]
+    fn matrix_multiplication_incompatible_size() {
+        let left = Matrix::<i32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let right = Matrix::<i32>::new(vec![vec![8, 7, 6], vec![5, 4, 3]]);
+        let _ = left * right;
+    }
+
+    #[test]
     fn scalar_product() {
-        let left = Vector::<u32>::new(&vec![0, 1, 2]);
-        let right = Vector::<u32>::new(&vec![3, 4, 5]);
-        assert_eq!(left * right, 14);
+        let left = Matrix::<u32>::vector(vec![0, 1, 2]).transpose();
+        let right = Matrix::<u32>::vector(vec![3, 4, 5]);
+        assert_eq!((left * right).to_scalar(), 14);
     }
 
     #[test]
     #[should_panic]
     fn matrix_creation_inconsistent_rows() {
-        let _ = Matrix::<u32>::new(&vec![vec![0, 1], vec![2, 3, 4, 5, 6]]);
+        let _ = Matrix::<u32>::new(vec![vec![0, 1], vec![2, 3, 4, 5, 6]]);
     }
 
     #[test]
     fn matrix_indexing() {
-        let matrix = Matrix::<u32>::new(&vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
+        let matrix = Matrix::<u32>::new(vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
         assert_eq!(matrix[0][0], 0);
         assert_eq!(matrix[0][1], 1);
         assert_eq!(matrix[0][2], 2);
@@ -213,23 +223,23 @@ mod tests {
     #[test]
     #[should_panic]
     fn matrix_indexing_out_of_bounds_row() {
-        let matrix = Matrix::<u32>::new(&vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
+        let matrix = Matrix::<u32>::new(vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
         let _ = matrix[2][0];
     }
 
     #[test]
     #[should_panic]
     fn matrix_indexing_out_of_bounds_col() {
-        let matrix = Matrix::<u32>::new(&vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
+        let matrix = Matrix::<u32>::new(vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
         let _ = matrix[1][5];
     }
 
     #[test]
     fn matrix_mut_indexing() {
-        let mut matrix = Matrix::<u32>::new(&vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
+        let mut matrix = Matrix::<u32>::new(vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]]);
         assert_eq!(
             matrix,
-            Matrix::<u32>::new(&vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]])
+            Matrix::<u32>::new(vec![vec![0, 1, 2, 3, 4], vec![5, 6, 7, 8, 9]])
         );
         matrix[0][0] = 9;
         matrix[0][1] = 8;
@@ -243,41 +253,41 @@ mod tests {
         matrix[1][4] = 0;
         assert_eq!(
             matrix,
-            Matrix::<u32>::new(&vec![vec![9, 8, 7, 6, 5], vec![4, 3, 2, 1, 0]])
+            Matrix::<u32>::new(vec![vec![9, 8, 7, 6, 5], vec![4, 3, 2, 1, 0]])
         );
     }
 
     #[test]
     fn matrix_negation() {
-        let matrix = Matrix::<i32>::new(&vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let matrix = Matrix::<i32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
         assert_eq!(
             -matrix,
-            Matrix::<i32>::new(&vec![vec![0, -1, -2], vec![-3, -4, -5], vec![-6, -7, -8]])
+            Matrix::<i32>::new(vec![vec![0, -1, -2], vec![-3, -4, -5], vec![-6, -7, -8]])
         );
     }
 
     #[test]
     fn matrix_addition() {
-        let left = Matrix::<u32>::new(&vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
-        let right = Matrix::<u32>::new(&vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
-        assert_eq!(left + right, Matrix::<u32>::new(&vec![vec![8, 8, 8]; 3]));
+        let left = Matrix::<u32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let right = Matrix::<u32>::new(vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
+        assert_eq!(left + right, Matrix::<u32>::new(vec![vec![8, 8, 8]; 3]));
     }
 
     #[test]
     fn matrix_addition_with_negation() {
-        let left = Matrix::<i32>::new(&vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
-        let right = Matrix::<i32>::new(&vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
+        let left = Matrix::<i32>::new(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]]);
+        let right = Matrix::<i32>::new(vec![vec![8, 7, 6], vec![5, 4, 3], vec![2, 1, 0]]);
         assert_eq!(
             left + -right,
-            Matrix::<i32>::new(&vec![vec![-8, -6, -4], vec![-2, 0, 2], vec![4, 6, 8]])
+            Matrix::<i32>::new(vec![vec![-8, -6, -4], vec![-2, 0, 2], vec![4, 6, 8]])
         );
     }
 
     #[test]
     #[should_panic]
     fn matrix_addition_incorrect_size() {
-        let left = Matrix::<u32>::new(&vec![vec![0, 1, 2]]);
-        let right = Matrix::<u32>::new(&vec![vec![3, 4, 5], vec![6, 7, 8]]);
+        let left = Matrix::<u32>::new(vec![vec![0, 1, 2]]);
+        let right = Matrix::<u32>::new(vec![vec![3, 4, 5], vec![6, 7, 8]]);
         let _ = left + right;
     }
 }
